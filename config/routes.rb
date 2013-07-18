bb35fa11566d8e884fb7a74b1ae5a32ce28d4a18
frehha <christian--wenzel@web.de>
2013-07-18 21:02:38 +0200

    only Download left

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 PubkeyApp::Application.routes.draw do
-	get "static_pages/home"
-	get "static_pages/help"
+	root to: 'static_pages#home'
 
+	match '/help',		to: 'static_pages#help',		via: 'get'
+	match '/about',		to: 'static_pages#about',		via: 'get'
+	match '/contact', to: 'static_pages#contact',	via: 'get'
+
+#	get "static_pages/home"
+#	get "static_pages/help"
+#	get "static_pages/about"
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -51,7 +57,7 @@ PubkeyApp::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  # root :to => 'static_pages#home'
 
   # See how all your routes lay out with "rake routes"
 
